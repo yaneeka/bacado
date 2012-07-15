e3d404f8fd2b1886d675fea1dff42e159f9b9121
Yaneeka Huq <yaneeka@stanfordalumni.org>
2012-07-15 15:01:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bacado::Application.config.secret_token = '350ffdcefd9591b05130ad892322a56cef4368654a7e453452020fb8e05fdea138506d6d5e07dcbdd11e0ce97e25861ea0421f698ddd998a2dd805c5539740d5'
